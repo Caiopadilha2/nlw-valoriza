1c98a60b95f7f7d4db55f281e68af5dfedbae0ca
Caio Padilha <caiopadilha@id.uff.br>
2023-04-22 22:17:08 -0300

    Rotas autenticadas, recebendo user_id pelo request

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,13 +1,23 @@
 import { Request, Response, NextFunction } from "express"
+import { getCustomRepository } from "typeorm";
+import { UserRepositories } from "../repositories/UsersRepositories";
 
-export function ensureAdmin(req: Request, res: Response, next: NextFunction) {
-  const admin = true;
-
-  if (admin) {
+export async function ensureAdmin(req: Request, res: Response, next: NextFunction) {
+  // const admin = true
+  // if (admin) {
+  //   return next();
+  // }
+  
+  const { user_id } = req;
+  // Verificar se esse usuário é um admin
+  const usersRepositories = getCustomRepository(UserRepositories);
+  const user = await usersRepositories.findOne(user_id);
+   
+  if (user.admin) {
     return next();
   }
 
   return res.status(401).json({
-    message: "Unauthorized!"
+    message: "You're not Admin!"
   })
 }
---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction, request } from "express"
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+  sub: string;
+}
+
+export function ensureAuthenticated(req: Request, res: Response, next: NextFunction){
+
+  // Receber o token
+  const authToken = req.headers.authorization;
+  //console.log(authToken);
+
+  const [ , token ] = authToken.split(" ")
+  // Split separa em 2 arrays
+  // console.log(token)
+  
+  // Verificar se token está preenchido
+  if (!authToken) {
+    return res.status(401).end();
+  }
+
+  try {
+    // Verificar se token é válido
+    // const decode = verify(token, "secret_key");
+    // console.log(decode)
+    // Recuperar informações do usuário
+    const { sub } = verify(token, "secret_key") as IPayload;
+    req.user_id = sub;
+  } catch (error) {
+    return res.status(401).end();
+  }
+  return next();
+}
+
